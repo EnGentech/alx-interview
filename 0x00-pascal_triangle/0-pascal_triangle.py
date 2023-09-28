34266e3b627e02929a69b2f623e228d6556e636b
EnGentech <engen.inyang@gmail.com>
2023-09-28 15:03:48 +0100

    interview question for pastal triangle solved

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Pascal triangle implementation
+"""
+
+
+def pascal_triangle(n):
+    tempList = [1, 2, 1]
+    pascal = [[1]]
+    if n == 1:
+        return pascal
+    elif n == 2:
+        pascal.append(tempList)
+        return pascal
+    elif n > 2:
+        pascal.append(tempList)
+        for x in range(2, n):
+            new_list = [1]
+            for tlist in range(len(tempList)):
+                if tlist != len(tempList) - 1:
+                    sum = tempList[tlist] + tempList[tlist + 1]
+                    new_list.append(sum)
+                elif tlist == len(tempList) - 1:
+                    new_list.append(1)
+                    pascal.append(new_list)
+                    tempList = new_list
+    return pascal
