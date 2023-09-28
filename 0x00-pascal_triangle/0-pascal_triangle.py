1e021e7b241a38c88fc777a6a59177ec6ca22cce
EnGentech <engen.inyang@gmail.com>
2023-09-28 15:33:27 +0100

    modified

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,26 +5,17 @@ Pascal triangle implementation
 
 
 def pascal_triangle(n):
+    """Pascal triangle implementation"""
+    pascal = []
     if n <= 0:
-        pascal = []
+        return pascal
     else:
-        tempList = [1, 1]
-        pascal = [[1]]
-        if n == 1:
-            return pascal
-        elif n == 2:
-            pascal.append(tempList)
-            return pascal
-        elif n > 2:
-            pascal.append(tempList)
-            for x in range(2, n):
-                new_list = [1]
-                for tlist in range(len(tempList)):
-                    if tlist != len(tempList) - 1:
-                        sum = tempList[tlist] + tempList[tlist + 1]
-                        new_list.append(sum)
-                    elif tlist == len(tempList) - 1:
-                        new_list.append(1)
-                        pascal.append(new_list)
-                        tempList = new_list
+        for x in range(n):
+            new_list = [1]
+            if pascal:
+                prev_list = pascal[-1]
+                for i in range(len(prev_list) - 1):
+                    new_list.append(prev_list[i] + prev_list[i + 1])
+                new_list.append(1)
+            pascal.append(new_list)
     return pascal
