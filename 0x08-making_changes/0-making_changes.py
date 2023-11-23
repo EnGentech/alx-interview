4e5d7d801d0be760f129fabeb9925907e15b1e0a
EnGentech <engen.inyang@gmail.com>
2023-11-23 07:01:56 +0100

    solved puzzle challenge

---
+++ 0x08-making_changes/0-making_changes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Creating solution to making changes challenge"""
+
+
+def make_change(coins, total):
+    """A function to determine the best possible changes"""
+    if total <= 0:
+        return 0
+
+    current_total = 0
+    used_coins = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = (total - current_total) // coin
+        current_total += r * coin
+        used_coins += r
+        if current_total == total:
+            return used_coins
+
+    return -1
---
+++ 0x08-making_changes/README.md
@@ -0,0 +1,3 @@
+This challenge is to determine the best
+possible way changes could be made to a coin
+else not valid return -1
